16dae24b0e5dc5701907a951d828fc1e5f16fdda
alka-siemens <alka.das@siemens-healthineers.com>
2023-03-01 14:09:24 +0530

    Section-3 complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Recipe 1',
+      'This is a Test Recipe',
+      'https://cdn.apartmenttherapy.info/image/fetch/t_sailthru_email_square/https://s3.amazonaws.com/pixtruder/original_images/f5cffedb779ce8ea3991f8020b5616d39ef6c0ee'
+    ),
+    new Recipe(
+      'Recipe 1',
+      'This is a Test Recipe',
+      'https://cdn.apartmenttherapy.info/image/fetch/t_sailthru_email_square/https://s3.amazonaws.com/pixtruder/original_images/f5cffedb779ce8ea3991f8020b5616d39ef6c0ee'
+    ),
+    new Recipe(
+      'Recipe 1',
+      'This is a Test Recipe',
+      'https://cdn.apartmenttherapy.info/image/fetch/t_sailthru_email_square/https://s3.amazonaws.com/pixtruder/original_images/f5cffedb779ce8ea3991f8020b5616d39ef6c0ee'
+    ),
+  ];
 }
